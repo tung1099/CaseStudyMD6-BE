a78d83c0fefd28e3a7c1fc9a9870ef0aea524cf7
tung1099 <dtroyal1102@gmail.com>
2022-06-08 13:51:21 +0700

    first

---
+++ src/main/java/com/codegym/castudymd6final/CastudyMd6FinalApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.castudymd6final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastudyMd6FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CastudyMd6FinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/castudymd6final/CastudyMd6FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.castudymd6final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastudyMd6FinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
