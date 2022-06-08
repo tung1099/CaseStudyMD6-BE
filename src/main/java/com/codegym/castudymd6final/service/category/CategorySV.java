47fbf27841e593abd09797cd9312fa55d8067de5
tung1099 <dtroyal1102@gmail.com>
2022-06-08 15:48:01 +0700

    cred category

--- src/main/java/com/codegym/castudymd6final/service/category/CategorySV.java
+++ src/main/java/com/codegym/castudymd6final/service/category/CategorySV.java
@@ -1,28 +1,35 @@
 package com.codegym.castudymd6final.service.category;
 
+import com.codegym.castudymd6final.model.entity.Category;
+import com.codegym.castudymd6final.repository.ICategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 @Service
 public class CategorySV implements ICategorySV{
+
+    @Autowired
+    private ICategoryRepo categoryRepo;
+
     @Override
-    public List findAll() {
-        return null;
+    public List<Category> findAll() {
+        return categoryRepo.findAll();
     }
 
     @Override
-    public Object save(Object o) {
-        return null;
+    public Category save(Category category) {
+        return categoryRepo.save(category);
     }
 
     @Override
     public void removeById(Long id) {
-
+        categoryRepo.deleteById(id);
     }
 
     @Override
-    public Optional findById(Long id) {
-        return Optional.empty();
+    public Optional<Category> findById(Long id) {
+        return categoryRepo.findById(id);
     }
 }
