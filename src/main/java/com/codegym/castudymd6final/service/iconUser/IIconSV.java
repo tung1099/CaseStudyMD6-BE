c6e8879c0a0c922e348a240da3bfacc4d1a8df8d
NguyenAnh1212 <96817453+NguyenAnh1212@users.noreply.github.com>
2022-06-11 13:40:17 +0700

    g

---
+++ src/main/java/com/codegym/castudymd6final/service/iconUser/IIconSV.java
@@ -0,0 +1,7 @@
+package com.codegym.castudymd6final.service.iconUser;
+
+import com.codegym.castudymd6final.model.entity.Icon;
+import com.codegym.castudymd6final.service.IGeneralService;
+
+public interface IIconSV extends IGeneralService<Icon> {
+}
