9a9902d76a9c84c492208e18f7128a49e2663758
CanhNguyen <leagueyasuo186@gmail.com>
2022-06-16 10:28:00 +0700

    fsdf

--- src/main/java/com/codegym/castudymd6final/service/addMoney/IAddMoneySV.java
+++ src/main/java/com/codegym/castudymd6final/service/addMoney/IAddMoneySV.java
@@ -3,8 +3,11 @@ package com.codegym.castudymd6final.service.addMoney;
 import com.codegym.castudymd6final.model.entity.AddMoney;
 import com.codegym.castudymd6final.service.IGeneralService;
 
+import java.util.Date;
 import java.util.List;
 
 public interface IAddMoneySV extends IGeneralService<AddMoney> {
     List<AddMoney> getAddMoneyByWallet(Long idWallet);
+
+    Iterable<AddMoney> getListAddMoneyInTimeByIdWallet(Date date1, Date date2, Long idWallet);
 }
